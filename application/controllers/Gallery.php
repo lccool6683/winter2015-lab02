dcc03634dc789bbdd908cdf4c3ff6732ef21992c
Clemens Lo <lsps9140814@gmail.com>
2015-01-17 23:21:17 -0800

    Generated Gallery Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -11,14 +11,33 @@
  *
  * @author Clemens
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Gallery extends CI_Controller{
+//defined('BASEPATH') OR exit('No direct script access allowed');
+class Gallery extends Application{
     //put your code here
     public function index()
 	{
-		//$this->load->view('gallery');
-                $this->data['pagebody'] = 'gallery';
-                $this->render();
+            //get all the images from our model
+            $pix = $this->images->all();
+            
+            //build an array of formatted cells for them
+            foreach ($pix as $picture)
+                $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        
+            //prime the table class
+            $this->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            $this->table->set_template($parms);
+            
+            //finally gen the table
+            $rows = $this->table->make_columns($cells, 3);
+            $this->data['thetable'] = $this->table->generate($rows);
+            
+            //old stuff
+            $this->data['pagebody'] = 'gallery';
+            $this->render();
 	}
 }
