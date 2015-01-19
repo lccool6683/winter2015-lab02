65469df33786597269a544778093682cef60fa4e
Clemens Lo <lsps9140814@gmail.com>
2015-01-19 15:51:27 -0800

    commit all missing files and option task file Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* core/MY_Controller.php
+*
+* Default application controller
+*/
+class Application extends CI_Controller {
+protected $data = array(); // parameters for view components
+protected $id; // identifier for our content
+protected $choices = array(// our menu navbar
+'Home' => '/', 'Gallery' => '/gallery', 'About' => '/about'
+);
+/**
+* Constructor.
+* Establish view parameters & load common helpers
+*/
+function __construct()
+{
+parent::__construct();
+$this->data = array();
+$this->data['pagetitle'] = 'Sample Image Gallery';
+}
+/**
+* Render this page
+*/
+function render()
+{
+$this->data['menubar'] = $this->build_menu_bar($this->choices);
+$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+$this->data['data'] = &$this->data;
+$this->parser->parse('_template', $this->data);
+}
+
+function build_menu_bar($choices)
+{
+    $result = '<ul>';
+    foreach ($choices as $name => $link)
+	$result .= '<li>' . anchor($link, $name) . '</li>';
+    $result .= '</ul>';
+    return $result;
+}
+}
+/* End of file MY_Controller.php */
+/* Location: application/core/MY_Controller.php */
---
+++ application/models/Images.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_Model
+ *
+ * @author Clemens
+ */
+class Images extends CI_Model {
+    //put your code here
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    //return all images, descending order by post date
+    function all()
+    {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    
+    //return just 3 newest images
+    function newest()
+    {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    
+}
